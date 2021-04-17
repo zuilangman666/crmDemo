bd2eebdabb07235d4e3802070f0297d14e75ff73
gyp <3182116235@qq.com>
2021-04-17 20:42:45 +0800

    第一次提交

---
+++ src/com/geng/domain/Student.java
@@ -0,0 +1,13 @@
+package com.geng.domain;
+
+public class Student {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
